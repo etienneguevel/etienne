45ef08c2d6fa4666a8b62e06e804870ceeed79ad
root <root@Phobos.localdomain>
2021-06-18 17:17:16 +0200

    Update 18/06

--- snakes/Etienne.sh
+++ snakes/Etienne.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
+snakemake -s snakes/Etienne.snake -p -n | grep "^python"
+snakemake -s snakes/Etienne.snake --cluster "sbatch --time 48:00:00 -J Etienne --ntasks-per-socket=4 --mem-per-cpu=32G -x w03 --output ./OUT_Etienne/Etienne%j.log --error ./ERR_Etienne/Etienne%j.log" -j1200
 
-snakemake -s snakes/Etienne.snake --cluster "sbatch --time 48:00:00 -J Etienne --ntasks-per-socket=4 --mem-per-cpu=32G -x w03,w05 --output ./OUT_Etienne/Etienne%j.log --error ./ERR_Etienne/Etienne%j.log" -j 64
